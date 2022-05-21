e5e0d60651122754f26dad37aff558c0fb9a797f
Dipesh Adhikari <dipeshadk77@gmail.com>
2022-05-21 16:52:05 +0545

    changecmmit

--- datatypes.py
+++ datatypes.py
@@ -10,4 +10,31 @@
 
 
 
+# Practice
+
+#int type
+a = 5
+b = 10000
+c = 123456789
+print(a,b,c)
+
+#float type
+a = 5.55567
+b = 3.0
+c = 55
+print(a,b,c)
+
+#complex type
+a = complex(5,6)
+b = complex(1.11,9.999)
+c = complex(99,100.00)
+print(a,b,c)
+
+#string type
+a = 'This is string type'
+b = "I'm also string"
+c = '''I'm another string"type'''
+print(a)
+print(b)
+print(c)
 
