7b45b0a9391993a960fd6af806be6d7bcc259d94
Dipesh Adhikari <dipeshadk77@gmail.com>
2022-05-21 16:42:38 +0545

    commit3

---
+++ datatypes.py
@@ -0,0 +1,13 @@
+# Every value in Python has a datatype. Since everything is an object in Python programming, data types are actually classes and variables are instance (object) of these classes.
+
+# Built-in data types in python
+
+# int, float, complex
+# str
+# list, tuple, range
+# set, dict
+# bool - True, False
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,38 @@
+
+#Variables
+# - are containers for storing data values that may take on one or more values during a program’s runtime. 
+# - is created the momemt value is assigned
+# - python has no command for declaring a variable
+
+
+# Naming a variable
+
+# - must be single word, consisting letters/numbers/_
+# - cannot begin with number
+# - Variables in Python which start with _ (underscore) are considered as “Unuseful”
+
+
+# Examples:
+
+#Error 5a = 10
+a = 5
+print(a)
+abc = "I'm a string type variable"
+print(abc)
+a55 = 'string type variable-2'
+print(a55)
+_a55 = 'Unuseful variable'
+print(_a55)
+
+print(abc + a55 + _a55)
+
+a=b=c = "one value to multiple variable"
+print('\na=b=c=', a)
+
+x, y, z = "many value ", 2, " multiple variable"
+print('\nx=',x, '\ny=',y, '\nz=',z)
+
+
+
+
+
